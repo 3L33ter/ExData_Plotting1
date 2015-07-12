a33e9abc496566f7fad0b7670ec32c76e53615c1
3L33ter <arash.ahani@gmail.com>
2015-07-11 21:32:15 -0700

    Here's my submission for assignment 1!

--- plot2.R
+++ plot2.R
@@ -1,14 +1,9 @@
 #only load dates 2007-02-01 and 2007-02-02
 #head is row 1, row 66638 1st row, row 69517 last row of the data I want
-#so keep head, skip 66636, and read 2880 rows
-
-#this gives the wrong header
-#data_1 <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, skip=66636, nrows=2880)
-
-#this works
+#read thru 2/2/2007
 data_1 <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, na.strings="?", nrows=69516)
 
-#we have days X thru 2/2/2007 so throw away the days we don't want
+#now throw away the days we don't want
 data <- data_1[66637:69516,]
 
 #paste concatenates the date and time columns, and strptime makes it into a date/time object for R
